fb8d6e56b6b8f60214295f162ff8cd7e0aab2da3
Supawitsup <supawit.sup@ku.th>
2019-03-22 14:03:51 +0700

    show car

--- Car.py
+++ Car.py
@@ -6,13 +6,20 @@ SCREEN_HEIGHT = 600
 class CarWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
+        arcade.set_background_color(arcade.color.GRAY)
+        
+        self.car_sprite = arcade.Sprite('images/motorbike.png', 0.1)
+        self.car_sprite.set_position(300,300)
+      
  
-        arcade.set_background_color(arcade.color.GREEN)
- 
+    def on_draw(self):
+        arcade.start_render()
+        self.car_sprite.draw()
+
 def main():
     window = CarWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
- 
+
 if __name__ == '__main__':
     main()
