bc47a4877fbac6889aa41a2992927b23a6435b6b
Supawitsup <supawit.sup@ku.th>
2019-04-05 13:20:31 +0700

    Create Second player

--- Car.py
+++ Car.py
@@ -5,7 +5,7 @@ SPRITE_SCALING = 0.1
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Move Sprite with Keyboard Example"
+
 
 MOVEMENT_SPEED = 5
 
@@ -26,90 +26,103 @@ class Player(arcade.Sprite):
         elif self.top > SCREEN_HEIGHT - 1:
             self.top = SCREEN_HEIGHT - 1
 
-
 class MyGame(arcade.Window):
     """
     Main application class.
     """
 
-    def __init__(self, width, height, title):
+    def __init__(self, width, height):
         """
         Initializer
         """
 
-        # Call the parent class initializer
-        super().__init__(width, height, title)
+        super().__init__(width, height)
 
-        # Set the working directory (where we expect to find files) to the same
-        # directory this .py file is in. You can leave this out of your own
-        # code, but it is needed to easily run the examples using "python -m"
-        # as mentioned at the top of this program.
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
 
-        # Variables that will hold sprite lists
-        self.player_list = None
 
-        # Set up the player info
-        self.player_sprite = None
+        self.player1_list = None
+        self.player1_sprite = None
 
-        # Set the background color
+        self.player2_list = None
+        self.player2_sprite = None
+        
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
         """ Set up the game and initialize the variables. """
 
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
 
-        # Set up the player
-        self.player_sprite = Player("images/motorbike.png", SPRITE_SCALING)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
+        self.player1_list = arcade.SpriteList()
+        self.player2_list = arcade.SpriteList()
+
+        self.player1_sprite = Player("images/motorbike.png", SPRITE_SCALING)
+        self.player1_sprite.center_x = 50
+        self.player1_sprite.center_y = 50
+        self.player1_list.append(self.player1_sprite)
+
+        self.player2_sprite = Player("images/motorbike.png", SPRITE_SCALING)
+        self.player2_sprite.center_x = 500
+        self.player2_sprite.center_y = 50
+        self.player2_list.append(self.player2_sprite)
+
 
     def on_draw(self):
         """
         Render the screen.
         """
 
-        # This command has to happen before we start drawing
+
         arcade.start_render()
 
-        # Draw all the sprites.
-        self.player_list.draw()
-
+        self.player1_list.draw()
+        self.player2_list.draw()
     def update(self, delta_time):
         """ Movement and game logic """
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-        self.player_list.update()
-
+        self.player1_list.update()
+        self.player2_list.update()
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
 
         if key == arcade.key.UP:
-            self.player_sprite.change_y = MOVEMENT_SPEED
+            self.player1_sprite.change_y = MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
-            self.player_sprite.change_y = -MOVEMENT_SPEED
+            self.player1_sprite.change_y = -MOVEMENT_SPEED
         elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
+            self.player1_sprite.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = MOVEMENT_SPEED
+            self.player1_sprite.change_x = MOVEMENT_SPEED
+
+
+        
+        if key == arcade.key.W:
+            self.player2_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.player2_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.A:
+            self.player2_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.player2_sprite.change_x = MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
 
         if key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player_sprite.change_y = 0
+            self.player1_sprite.change_y = 0
         elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player_sprite.change_x = 0
+            self.player1_sprite.change_x = 0
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player2_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player2_sprite.change_x = 0
 
 
 def main():
     """ Main method """
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
     window.setup()
     arcade.run()
 
